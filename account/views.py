08e17ee40e185bd99e07820aade0eada1176948b
DBWtv <dima.bwc@gmail.com>
2022-12-13 11:21:15 +0600

    view for registration

--- account/views.py
+++ account/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create new account without saving in DB
+            new_user = user_form.save(commit=False)
+            # Создаем зашифрованный пароль
+            new_user.set_password(user_form.cleaned_data['password'])
+            # savin in DB
+            new_user.save()
+        return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 @login_required
 def dashboard(request):
