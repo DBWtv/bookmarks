1cbb78b163b8f5126e1e063bd0e26466671eb060
DBWtv <dima.bwc@gmail.com>
2022-12-13 17:02:57 +0600

    fix bug

--- images/views.py
+++ images/views.py
@@ -5,18 +5,26 @@ from .forms import ImageCreateForm
 
 @login_required
 def image_create(request):
-    # form send
-    form = ImageCreateForm(data=request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        new_item = form.save(commit=False)
-        new_item.user = request.user
-        new_item.save()
-        messages.success(request, 'Image aded succefuly')
-        return redirect(new_item.get_absolute_url())
+    """
+    View for creating an Image using the JavaScript Bookmarklet.
+    """
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            # redirect to new created item detail view
+            return redirect(new_item.get_absolute_url())
     else:
+        # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-    return render( request, 'iamges/image/create.html',
-                            {'sections': 'images', 'form': form})
 
+    return render(request, 'images/image/create.html', {'section': 'images',
+                                                        'form': form})
 
