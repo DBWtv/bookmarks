2d75843e8e33bdf0d05099bd2f029d55e47fd98d
DBWtv <dima.bwc@gmail.com>
2022-12-13 11:28:57 +0600

    registration templates

--- account/views.py
+++ account/views.py
@@ -15,7 +15,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # savin in DB
             new_user.save()
-        return render(request, 'account/register_done.html', {'new_user': new_user})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
