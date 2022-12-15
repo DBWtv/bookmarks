9f7d6057f8aba982b3da4e4d11712c1b1a8a5e96
DBWtv <dima.bwc@gmail.com>
2022-12-15 10:36:57 +0600

    fix bugs

--- account/views.py
+++ account/views.py
@@ -13,16 +13,6 @@ from .models import Contact
 from actions.utils import create_action
 from actions.models import Action
 
-@login_required
-def dashboard(request):
-    actions = Action.objects.exclude(user=request.user)
-    following_ids = request.user.following.values_list('id', flat=True)
-    if following_ids:
-        actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__profile')\
-                     .prefetch_related('target')[:10]
-    return render(request, 'account/dashboard.html',
-                    {'section': 'dashboard', 'actions': actions})
 
 @ajax_required
 @require_POST
@@ -81,8 +71,17 @@ def register(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+    print(actions)
+    return render(request, 'account/dashboard.html',
+                    {'section': 'dashboard', 'actions': actions})
 
+                    
 @login_required
 def edit(request):
     if request.method == 'POST':
