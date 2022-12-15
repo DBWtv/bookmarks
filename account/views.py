4c499c383d694e216ebbea6dac3d6ce4bced1a8f
DBWtv <dima.bwc@gmail.com>
2022-12-15 10:17:11 +0600

    making actions tracking for user

--- account/views.py
+++ account/views.py
@@ -10,6 +10,19 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
+
+@login_required
+def dashboard(request):
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html',
+                    {'section': 'dashboard', 'actions': actions})
 
 @ajax_required
 @require_POST
@@ -24,6 +37,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
+                create_action(request.user, 'is following', user)
                 return JsonResponse({'status':'ok'})
         except User.DoesNotExist:
             return JsonResponse({'status':'ok'})
@@ -59,7 +73,7 @@ def register(request):
             # savin in DB
             new_user.save()
             Profile.objects.create(user=new_user)
-            
+            create_action(new_user, 'has createdd an account')            
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
--- images/views.py
+++ images/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -40,6 +41,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
@@ -59,6 +61,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
